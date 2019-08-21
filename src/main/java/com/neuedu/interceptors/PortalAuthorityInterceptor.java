39d170ce02f7428f1b7c6a1146433219886dd30a
Koukouyi710 <yangmh@qq.com>
2019-08-21 11:10:04 +0800

    完成工具类封装，优化拦截器

--- src/main/java/com/neuedu/interceptors/PortalAuthorityInterceptor.java
+++ src/main/java/com/neuedu/interceptors/PortalAuthorityInterceptor.java
@@ -3,6 +3,7 @@ package com.neuedu.interceptors;
 import com.neuedu.common.Const;
 import com.neuedu.common.ServerResponse;
 import com.neuedu.pojo.UserInfo;
+import com.neuedu.utils.JsonUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -30,8 +31,8 @@ public class PortalAuthorityInterceptor implements HandlerInterceptor{
                 response.setHeader("Content-Type","application/json;charset=UTF-8");
                 PrintWriter printWriter = response.getWriter();
                 ServerResponse serverResponse = ServerResponse.createServerResponseByFail(Const.ResponseCodeEunm.NEED_LOGIN.getCode(),Const.ResponseCodeEunm.NEED_LOGIN.getDesc());
-                //String json = JSONUtils.toJSONString(serverResponse);
-                String json = "需要登录";
+                String json = JsonUtils.obj2String(serverResponse);
+                //String json = "需要登录";
                 printWriter.write(json);
                 printWriter.flush();
                 printWriter.close();
--- src/main/java/com/neuedu/utils/JsonUtils.java
+++ src/main/java/com/neuedu/utils/JsonUtils.java
@@ -1,8 +1,19 @@
 package com.neuedu.utils;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import com.neuedu.pojo.UserInfo;
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.type.JavaType;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -10,12 +21,117 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
  */
 
 public class JsonUtils {
-   /* private static ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper objectMapper = new ObjectMapper();
     static {
         //对象中所有的字段序列化
         objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.ALWAYS);
         //取消默认timestamp格式
         objectMapper.configure(SerializationConfig.Feature.WRITE_DATE_KEYS_AS_TIMESTAMPS,false);
+        //忽略空bean转json错误
+        objectMapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS,false);
+        //设置日期格式
+        objectMapper.setDateFormat(new SimpleDateFormat(DateUtils.STANDARD_FORMAT));
+        //忽略在json中存在，但是在java中不存在的属性，防止出错
+        objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+    }
 
+    /**
+     * 将对象转成字符串
+     */
+    public static <T> String obj2String(T obj){
+        if (obj==null){
+            return null;
+        }
+        try {
+            return obj instanceof String?(String) obj:objectMapper.writeValueAsString(obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将对象转成字符串
+     */
+    public static <T> String obj2StringPretty(T obj){
+        if (obj==null){
+            return null;
+        }
+        try {
+            return obj instanceof String?(String) obj:objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 字符串转对象
+     */
+    public static <T> T str2Obj(String str,Class<T> tclass){
+        if (StringUtils.isEmpty(str)||tclass==null){
+            return null;
+        }
+        try {
+            return tclass.equals(String.class)?(T)str:objectMapper.readValue(str,tclass);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *将json数组转换为集合
+     */
+    public static <T> T str2Obj(String str, TypeReference<T> typeReference){
+        if (StringUtils.isEmpty(str)||typeReference==null){
+            return null;
+        }
+        try {
+            return typeReference.getType().equals(String.class)?(T)str:objectMapper.readValue(str,typeReference);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     *将json数组转换为Map集合
+     */
+    public static <T> T str2Obj(String str, Class<?> collectionClass,Class<?>...elements){
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass,elements);
+        try {
+            return objectMapper.readValue(str,javaType);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+   /* public static void main(String[] args) {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setId(1);
+        userInfo.setUsername("31wsawd单丝");
+        userInfo.setEmail("1312@qq.com");
+        userInfo.setCreateTime(new Date());
+        String json = obj2String(userInfo);
+        UserInfo userInfo1 = new UserInfo();
+        userInfo1.setId(2);
+        userInfo1.setUsername("31wsawd单丝");
+        userInfo1.setEmail("1312@qq.com");
+        userInfo1.setCreateTime(new Date());
+        String json1 = obj2String(userInfo1);
+        List<UserInfo>userInfoList = Lists.newArrayList();
+        userInfoList.add(userInfo);
+        userInfoList.add(userInfo1);
+
+        String s = obj2StringPretty(userInfoList);
+        List<UserInfo>userInfoList1 = str2Obj(s, new TypeReference<List<UserInfo>>() {
+        });
+
+        List<UserInfo>userInfoList2 = str2Obj(s,List.class,UserInfo.class);
+
+        System.out.println(s);
+        System.out.println(userInfoList2);
     }*/
 }
